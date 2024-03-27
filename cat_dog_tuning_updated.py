b69ef2b1b4449813afba1eda102b4a88a49dcf57
wgreunke <wgreunke@gmail.com>
2024-03-26 18:31:23 -0700

    sdf

--- cat_dog_tuning_updated.py
+++ cat_dog_tuning_updated.py
@@ -37,6 +37,15 @@ print(np.min(first_image),np.max(first_image))
 
 
 #Now create the model
+#Fetch existing model
+from tensorflow.keras.applications.vgg16 import VGG16
+
+base_model = VGG16(input_shape = (224, 224, 3), # Shape of our images
+include_top = False, # Leave out the last fully connected layer
+weights = 'imagenet')
+
+
 #First freeze layers that will stay the same
+
 
 
